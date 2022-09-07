2e589662d6acada71e41608dc299ebd0b3e75908
EC2 Default User <ec2-user@ip-172-31-35-87.us-east-2.compute.internal>
2022-09-07 15:36:35 +0000

    changed modulation1

---
+++ file1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Program name: "printVar.sh"
+# Linux shell script program to create and print the value of variables.
+
+country="India"
+year=2021
+
+echo "Country name: $country"
+echo "Year: $year"
